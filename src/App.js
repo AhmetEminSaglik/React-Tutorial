f8785cb3a98bac95b13dab78cfec579dee2d2621
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2023-07-08 16:39:09 +0300

    bootstrap is added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 //import logo from './logo.svg';
 import "./App.css";
-import User from "./component/User.js";
 
 function App() {
-  const test = 34;
   return (
-    <div className="App">
-      <h3>App Component</h3>
-      <User />
-      <User />
-      <User />
-
-      <h1>===========================================</h1>
-      <h1>Hello React</h1>
-      <h1>1+1 </h1>
-      <h1>{1 + 1}</h1>
-
-      <h1 className="test">Hello Reac2t - className</h1>
-      <h1 htmlFor="test">Hello Reac2t - htmlFor</h1>
-      <h1>Hello React</h1>
-      <h2>{"Ahmet Emin".toUpperCase()}</h2>
-      <h4>{test}</h4>
+    <div className="container">
+      <h4 className="header"> App Component</h4>
+      <h4 style={{ color: "red", fontSize: "30px" }}> App Component</h4>
     </div>
   );
 }
