8a8b05af6ff959e1fef6a7c911731695d1b99d53
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2023-07-08 20:32:30 +0300

    card and icon are used

--- src/component/User.js
+++ src/component/User.js
@@ -6,6 +6,9 @@ class User extends Component {
   // this is class not function so here the props can be used as this.props
   //Destructing --> this prevents to add prefix for each variable as this.props
 
+  //static defaultProps = {...}
+  // or
+  // User.defaultProps = {...}
   static defaultProps = {
     name: "No Information 2",
     departmant: "No Information 2",
@@ -14,13 +17,25 @@ class User extends Component {
   render() {
     const { name, departmant, salary } = this.props;
     return (
-      <div>
-        <ul>
-          <li>Name : {name}</li>
-          <li>Departman : {departmant}</li>
-          <li>Salary : {salary}</li>
-        </ul>
+      <div className="col-md-8 mb-4">
+        <div className="card">
+          <div className="card-header d-flex justify-content-between">
+            <h4 className="d-inline">{name}</h4>
+            <i className="far fa-trash-alt" style={{ cursor: "pointer" }}></i>
+          </div>
+          <div className="card-body">
+            <p className="card-text">Salary :{salary}</p>
+            <p className="card-text">Departmant :{departmant}</p>
+          </div>
+        </div>
       </div>
+      // <div>
+      //   <ul>
+      //     <li>Name : {name} <i className="far fa-trash-alt"></i></li>
+      //     <li>Departman : {departmant}</li>
+      //     <li>Salary : {salary}</li>
+      //   </ul>
+      // </div>
     );
   }
 }
