1a58dd4da928fbcaad4de40999ee52e73b6f625d
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2023-07-08 16:46:43 +0300

    Navbar is added. rfc

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 //import logo from './logo.svg';
 import "./App.css";
-
+import Navbar from "./component/Navbar";
 function App() {
   return (
     <div className="container">
+      <Navbar/>
       <h4 className="header"> App Component</h4>
       <h4 style={{ color: "red", fontSize: "30px" }}> App Component</h4>
     </div>
---
+++ src/component/Navbar.js
@@ -0,0 +1,13 @@
+//rfc
+import React from 'react'
+
+// export default function Navbar() {  or use the following code 
+const Navbar = () => {
+  return (
+    <div>
+      <h2>Navbar Demo</h2>
+    </div>
+  )
+}
+
+export default Navbar;
