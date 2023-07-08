0de477b14583152dcadaef00094679bd233dc780
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2023-07-08 16:19:38 +0300

    created User component and used it multiple times

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 //import logo from './logo.svg';
 import "./App.css";
+import User from "./component/User.js";
 
 function App() {
+  const test = 34;
   return (
     <div className="App">
+      <h3>App Component</h3>
+      <User />
+      <User />
+      <User />
+
+      <h1>===========================================</h1>
       <h1>Hello React</h1>
+      <h1>1+1 </h1>
+      <h1>{1 + 1}</h1>
+
       <h1 className="test">Hello Reac2t - className</h1>
       <h1 htmlFor="test">Hello Reac2t - htmlFor</h1>
       <h1>Hello React</h1>
+      <h2>{"Ahmet Emin".toUpperCase()}</h2>
+      <h4>{test}</h4>
     </div>
   );
 }
---
+++ src/component/User.js
@@ -0,0 +1,17 @@
+//rcc
+import React, { Component } from "react";
+
+class User extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" />
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default User;
