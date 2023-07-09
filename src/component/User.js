b79d08d671cf53d2ee36df910b32ecdf7a89fdef
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2023-07-09 09:51:27 +0300

    note is added how to use for Provider and Consumer.

--- src/component/User.js
+++ src/component/User.js
@@ -5,24 +5,14 @@ import PropTypes from "prop-types";
 class User extends Component {
   // this is class not function so here the props can be used as this.props
   //Destructing --> this prevents to add prefix for each variable as this.props
-
   state = {
     isVisible: false,
   };
-
-  //  bind this  in constructor :
-  // constructor(props) {
-  //   super(props);
-  //   this.OnClickEvent=this.OnClickEvent.bind(this);
-  // }
   OnClickEvent = (number, e) => {
     this.setState({ isVisible: !this.state.isVisible });
     console.log(e);
   };
-  onDeleteUser = (e) => {
-    const { id} = this.props;
-    //Consumer Dispatch
-  };
+
   render() {
     const { name, departmant, salary } = this.props;
     const { isVisible } = this.state;
@@ -31,11 +21,8 @@ class User extends Component {
         <div className="card">
           <div className="card-header d-flex justify-content-between">
             <h4 className="d-inline" onClick={this.OnClickEvent.bind(this, 34)}>
-              {/* must be used bind although we create arrow function to send variable in OnClickEvent */}
               {name}
             </h4>
-            {/* bind : When we create functions, we have to bind like onClick example --> this.OnClickEvent.bind(this)
-            or can be used in a constructor. */}
             <i
               className="far fa-trash-alt"
               onClick={this.onDeleteUser}
@@ -59,10 +46,5 @@ User.propTypes = {
   departmant: PropTypes.string.isRequired,
   salary: PropTypes.string.isRequired,
 };
-// create default props
-// User.defaultProps = {
-//   name: "No Information 1",
-//   departmant: "No Information 1",
-//   salary: "No Information 1",
-// };
+
 export default User;
